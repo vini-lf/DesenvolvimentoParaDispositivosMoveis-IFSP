bd883c9d9913118d4104beba0795eb63946ba493
vini-lf <vinicius.lealferreira@outlook.com>
2026-09-09 20:20:12 -0300

    Delete lib directory

--- lib/questoes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_application_1/pergunta.dart';
-
-const questoes = [
-  Pergunta('1 + 2', ['3', '2', '4', '5']),
-  Pergunta('2 + 3', ['5', '2', '4', '3']),
-  Pergunta('10 - 4', ['6', '4', '8', '5']),
-  Pergunta('3 * 3', ['9', '6', '12', '3']),
-  Pergunta('8 / 2', ['4', '2', '8', '16']),
-];
